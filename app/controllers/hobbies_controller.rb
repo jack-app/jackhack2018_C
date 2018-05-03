704d1075bbcdba7dc3018eb756be34bd8a70b03e
yushi_jack <ihsuyadihcu@gmail.com>
2018-05-04 08:37:48 +0900

    hobbiesコントローラーのviewとactionを修正（追加）

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,11 +1,21 @@
 class HobbiesController < ApplicationController
   def index
     @hobbies = Hobby.all
+  end
+
+  def new
+    @showHobby = Hobby.offset(rand(Hobby.count)).first
     @hobby = Hobby.new
   end
 
   def create
     @hobby = Hobby.create(params.require(:hobby).permit(:name))
+    redirect_to root_path
+  end
+
+  def destroy
+    @hobby = Hobby.find(params[:id])
+    @hobby.destroy
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'hobbies/index'
-  post 'hobbies/index' => 'hobbies#create'
+  post 'hobbies/index' => 'hobbies#destroy'
+  get 'hobbies/new'
+  post 'hobbies/new' => 'hobbies#create'
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
-  root to: 'hobbies#index'
+  root to: 'hobbies#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
